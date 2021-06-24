790f0d81861371a3f022ef850e57bdcc75759a05
ouyang <wenjiao8605@126.com>
2021-06-24 08:32:57 +0800

    导航

--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -1,6 +1,26 @@
 <template>
     <el-container>
-        <el-header>1111</el-header>
+        <el-header>
+            <el-row class="header-box">
+                <el-col :span="12" class="menu-col">
+                    <el-menu :default-active="activeIndex" class="tab-menu" mode="horizontal" @select="handleSelect">
+                        <el-menu-item index="1">首页</el-menu-item>
+                        <el-menu-item index="2">招聘信息</el-menu-item>
+                        <el-menu-item index="3">简历库</el-menu-item>
+                        <el-menu-item index="3">挂靠价格</el-menu-item>
+                    </el-menu>
+                </el-col>
+                <el-col :span="12" class="tag-col">
+                    <el-row class="row-bg">
+                        <el-col :span="3"><div class="tag-item">上传简历</div></el-col>
+                        <el-col :span="3"><div class="tag-item">我要找工作</div></el-col>
+                        <el-col :span="3"><div class="tag-item">我要招聘</div></el-col>
+                        <el-col :span="3"><div class="tag-item">注册</div></el-col>
+                        <el-col :span="3"><div class="tag-item">登录</div></el-col>
+                    </el-row>
+                </el-col>
+            </el-row>
+        </el-header>
         <el-main>
             <router-view></router-view>
         </el-main>
@@ -9,10 +29,64 @@
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        data() {
+            return {
+                activeIndex: '1'
+            }
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log('aa', key, keyPath);
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .el-header {
+        height: 63px !important;
+        background: #202329;
+    }
+    .el-menu.el-menu--horizontal {
+        border: none;
+    }
+    .el-menu {
+        background: #202329;
+        color: #fff;
+        font-size: 15px;
+        font-family: Microsoft YaHei;
+        font-weight: 400;
+    }
+    .el-menu--horizontal>.el-menu-item.is-active {
+        border: none;
+        color: #1E87F0;
+    }
+    .header-box {
+        height: 100%;
 
+        .el-col {
+            height: 100%;
+
+            .row-bg {
+                width: 100%;
+                height: 100%;
+
+                .tag-item {
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    font-size: 15px;
+                    font-family: Microsoft YaHei;
+                    font-weight: 400;
+                    color: #FFFFFF;
+                }
+            }
+        }
+    }
+    .menu-col {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 
 // 引入element-ui
-import { Container, Header, Main, Button, Row } from 'element-ui'
+import { Container, Header, Main, Menu, MenuItem, Row, Col, Button } from 'element-ui'
 Vue.use(Container)
 Vue.use(Header)
 Vue.use(Main)
-Vue.use(Button)
+Vue.use(Menu)
+Vue.use(MenuItem)
 Vue.use(Row)
+Vue.use(Col)
+Vue.use(Button)
 
 import App from './App.vue'
 import router from './router'
